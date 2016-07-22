110cda9006f9b6bae50e935e4064e42c7899256a
RomanPloschynsky <romansysyuk@gmail.com>
2016-07-22 16:42:15 +0300

    init

---
+++ src/Runer.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.time.*;
+
+/**
+ * Just run the homework programs
+ */
+
+public class Runer {
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        //Lection 1
+// 1. Palindrome:
+//        while (scnr.hasNextInt()){
+//            String nmbr = String.valueOf(scnr.nextInt());
+//            System.out.println("Is number " + nmbr + " a palindrome? " + Palindromes.isPalindrome(nmbr));
+//        }
+
+// 2. Month:
+//        MonthNameFromNumber.getMonthName(scnr.nextLine());
+
+// 4. Rectangles:
+//        RectangleRanner.getMssgFromRectangles(10);
+
+// 3. Matrix
+//        Matrix.presentation(6);
+
+        //Lection 2
+// Encapsulation with dogs
+//        Dog dog = new Dog("Snooppy","Jack");
+//        dog.bark();
+//        dog.setName("Skooby");
+//        dog.setOwnerName("John");
+//        dog.bark();
+//        System.out.println(dog);
+
+// 1. Immutable Point, Line
+//        1. Создать список разных линий.
+//        2. Посчитать суммарный размер всех линий.
+//        3. Найти самую длинную линию.
+
+// 2. Books
+//        a)  список книг заданного автора;
+//        b)  список книг, выпущенных заданным издательством;
+//        c)  список книг, выпущенных после заданного года.
+
+    }
+}
+
+
+
