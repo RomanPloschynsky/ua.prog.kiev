d59a571393963ef7a9cf55c2d6659f1b146dd8c3
RomanPloschynsky <romansysyuk@gmail.com>
2016-07-22 16:42:52 +0300

    init

---
+++ src/Animal.java
@@ -0,0 +1,18 @@
+/**
+ * Created by exite on 21.07.16.
+ */
+public class Animal {
+    private String animalType;
+
+    public Animal(String animalType) {
+        this.animalType = animalType;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+}
